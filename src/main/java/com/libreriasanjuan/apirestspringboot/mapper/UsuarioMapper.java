4fb16a763e46a0e25f61e1c865b5aa2d07242eb6
LucasSansberro <lucas_sansberro@outlook.com>
2023-03-24 18:20:59 -0300

    Bug in mapper fixed

--- src/main/java/com/libreriasanjuan/apirestspringboot/mapper/UsuarioMapper.java
+++ src/main/java/com/libreriasanjuan/apirestspringboot/mapper/UsuarioMapper.java
@@ -3,6 +3,7 @@ package com.libreriasanjuan.apirestspringboot.mapper;
 import com.libreriasanjuan.apirestspringboot.dto.UsuarioDTO;
 import com.libreriasanjuan.apirestspringboot.models.Usuario;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
@@ -10,6 +11,7 @@ import java.util.List;
 @Mapper(componentModel = "spring")
 public interface UsuarioMapper {
 
+    @Mapping(source = "isAdmin", target = "admin")
     UsuarioDTO BDaDTO(Usuario usuario);
 
     List<UsuarioDTO> convertirListaADTO(List<Usuario> usuarios);
